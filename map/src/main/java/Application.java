5b4a2ba94886366c43068d852a252c97f028b28b
mizhidan <dj9824@126.com>
2020-02-29 18:01:30 +0800

    [danjie] feat: print students

--- map/src/main/java/Application.java
+++ map/src/main/java/Application.java
@@ -1,6 +1,23 @@
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 public class Application {
 
   public static void main(String[] args) {
+    Student Linda = new Student("Linda", 18);
+    Student Cindy = new Student("Cindy", 19);
+    Student Mary = new Student("Mary", 19);
+    Map<String, Integer> map = new LinkedHashMap<>();
+    map.put(Linda.getName(),Linda.getAge());
+    map.put(Cindy.getName(),Cindy.getAge());
+    map.put(Mary.getName(),Mary.getAge());
+    Teacher Bob = new Teacher(map, "Bob");
+    Student Ben = new Student("Ben", 19);
+    Student Solider = new Student("Solider", 20);
+    Bob.addStudent(Ben);
+    Bob.addStudent(Solider);
+    Bob.printAllStudents();
   }
 
 }
--- map/src/main/java/Teacher.java
+++ map/src/main/java/Teacher.java
@@ -1,3 +1,25 @@
+import java.util.Iterator;
+import java.util.Map;
+
 public class Teacher {
+    private Map<String, Integer> students;
+    private String name;
+
+    public Teacher(Map<String, Integer> students, String name) {
+        this.students = students;
+        this.name = name;
+    }
+
+    public void addStudent(Student stu) {
+        students.put(stu.getName(),stu.getAge());
+    }
+
+    public void printAllStudents() {
+        Iterator<Map.Entry<String, Integer>> iterator = students.entrySet().iterator();
+        while(iterator.hasNext()) {
+            Map.Entry<String, Integer> student = iterator.next();
+            System.out.println(student.getKey() + "," + student.getValue());
+        }
+    }
 
 }
